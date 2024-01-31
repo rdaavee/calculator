5b1a5e294eb59b23d8fd082f36e1afdcf3ad222c
ran <rdavee0412@gmail.com>
2024-01-31 22:36:28 +0800

    initial commit

---
+++ src/components/main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let input = document.getElementById('call');
+    let buttons = document.querySelectorAll('button');
+    let string = "";
+
+    buttons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            if (e.target.innerHTML == '=') {
+                try {
+                    string = eval(string);
+                    input.value = string;
+                } catch (error) {
+                    input.value = 'Error';
+                }
+            } else if (e.target.innerHTML == 'AC') {
+                string = "";
+                input.value = string;
+            } else if (e.target.innerHTML == 'Del') {
+                string = string.substring(0, string.length - 1);
+                input.value = string;
+            } else {
+                string += e.target.innerHTML;
+                input.value = string;
+            }
+        });
+    });
+});
